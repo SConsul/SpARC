4a862333335c92cf18501ec27659e8cbdcc9130a
Samar Khanna <samar.khanna@gmail.com>
2022-02-17 23:18:20 -0800

    Train/val/test splits

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,9 @@
 import json
+import random
+from collections import defaultdict, namedtuple
+
+
+data_row = namedtuple('DataRow', ['question', 'answer', 'source', 'target'])
 
 
 def parse_source_target(source, target):
@@ -26,7 +31,7 @@ def parse_source_target(source, target):
 
 
 def process_c_graph(c_graph):
-    data = []
+    data = defaultdict(set)
     nodes = c_graph['nodes']
     edges = c_graph['links']
 
@@ -34,20 +39,49 @@ def process_c_graph(c_graph):
         s, t = (e['source'], e['target']) if e['direction'] == 'forward' else (e['target'], e['source'])
         impl = e['weight']
 
-        row = {'question': parse_source_target(s, t), 'answer': 'yes' if impl == 'yes_yes' else 'no'}
-        data.append(row)
+        row = data_row(question=parse_source_target(s, t), answer='yes' if impl == 'yes_yes' else 'no',
+                       source=s, target=t)
+        # row = {'question': parse_source_target(s, t), 'answer': 'yes' if impl == 'yes_yes' else 'no',
+               # 'source': s, 'target': t}
+        data[s].add(row)
 
+    data = {n: [q._asdict() for q in qs] for n, qs in data.items()}
     return data
 
 
 def process_silver_facts(silver_facts):
-    data = []
+    data = defaultdict(set)
     for source, targets in silver_facts.items():
         for target, label in targets.items():
-            row = {'question': parse_source_target('IsA,' + source, target), 'answer': label}
-            data.append(row)
+            row = data_row(question=parse_source_target('IsA,' + source, target), answer=label,
+                           source='IsA,' + source, target=target)
+            data['IsA,' + source].add(row)
 
+    data = {n: [q._asdict() for q in qs] for n, qs in data.items()}
     return data
+
+
+def data_split(data, train=0.8, val=0.1, test=0.1):
+    train_data = defaultdict(list)
+    val_data = defaultdict(list)
+    test_data = defaultdict(list)
+
+    for n, questions in data.items():
+        q_prime = questions.copy()
+        random.shuffle(q_prime)
+        tr_a, tr_b = 0, int(train * len(q_prime))
+        val_a, val_b = tr_b, tr_b + int(val * len(q_prime))
+        test_a, test_b = val_b, len(q_prime)
+
+        train_data[n].extend(q_prime[tr_a:tr_b])
+        val_data[n].extend(q_prime[val_a:val_b])
+        test_data[n].extend(q_prime[test_a:test_b])
+
+    return train_data, val_data, test_data
+
+
+def flatten(l):
+    return [x for sub_l in l for x in sub_l]
 
 
 if __name__ == "__main__":
@@ -60,14 +94,25 @@ if __name__ == "__main__":
     c_data = process_c_graph(c_graph)
     s_data = process_silver_facts(facts)
 
+    train_c, val_c, test_c = data_split(c_data)
+    train_s, val_s, test_s = data_split(s_data)
+
     with open('beliefbank-data-sep2021/constraints_qa.json', 'w') as f:
         json.dump(c_data, f)
 
     with open('beliefbank-data-sep2021/silver_qa.json', 'w') as f:
-        json.dump(c_data, f)
+        json.dump(s_data, f)
 
     with open('beliefbank-data-sep2021/qa.json', 'w') as f:
-        json.dump(c_data + s_data, f)
+        json.dump(flatten(c_data.values()) + flatten(s_data.values()), f, indent=1)
 
+    with open('beliefbank-data-sep2021/qa_train.json', 'w') as f:
+        json.dump(flatten(train_c.values()) + flatten(train_s.values()), f, indent=1)
+
+    with open('beliefbank-data-sep2021/qa_val.json', 'w') as f:
+        json.dump(flatten(val_c.values()) + flatten(val_s.values()), f, indent=1)
+
+    with open('beliefbank-data-sep2021/qa_test.json', 'w') as f:
+        json.dump(flatten(test_c.values()) + flatten(test_s.values()), f, indent=1)
 
 
