18b8c8ee4e1c393fd9316f49f784ff9cf0fb9513
sconsul <sarthakc@stanford.edu>
2022-02-28 15:12:06 -0800

    refactored code structure, added tensorboard

--- src/train.py
+++ src/train.py
@@ -1,9 +1,30 @@
 import os
 import torch
+import argparse
 from tqdm import tqdm
 import torch.optim as optim
 from torch.utils.data.dataloader import DataLoader
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from utils.dataset import QADataset
+from torch.utils.tensorboard import SummaryWriter
 
+def passed_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_path', default="./beliefbank-data-sep2021/qa.json")
+    parser.add_argument('--max_epochs', type=int, default=10)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--lr', type=float, default=3e-4)
+    parser.add_argument('--lr_decay', type=bool, default=False)
+    parser.add_argument('--weight_decay', type=float, default=0.1)
+    parser.add_argument('--model_path', default="runs/baseline")
+    parser.add_argument('--num_workers', type=int, default=4)
+    parser.add_argument('--l1_reg', type=float, default=None)
+    parser.add_argument('--freeze_backbone', action='store_true', default=False)
+    parser.add_argument('--adapter', action='store_true', default=False)
+    # Options: lm_head, encoder.final_layer_norm, etc
+    parser.add_argument('--layer_names', nargs='+', type=str, default=[])
+    args = parser.parse_args()
+    return args
 
 def train(model, train_dataset, config):
     if config['adapter']:
@@ -61,6 +82,8 @@ def train(model, train_dataset, config):
                     layer.register_forward_hook(get_activation(name))
                     l1_layers.append(name)
 
+    writer = SummaryWriter()
+
     for epoch in range(config['max_epochs']):
         pbar = tqdm(enumerate(train_dataloader), total=len(train_dataloader))
         for it, (x, a, y) in pbar:
@@ -86,8 +109,50 @@ def train(model, train_dataset, config):
 
             # report progress
             pbar.set_description(f"epoch {epoch + 1} iter {it}: train loss {loss.item():.5f}")
+            writer.add_scalar("Loss/train", loss.item(), epoch)
 
         # save checkpoint
         if (epoch % 1) == 0:
             model_path = os.path.join(config['model_path'], f"{epoch + 1}.bin")
             torch.save(model.state_dict(), model_path)
+
+    writer.flush()
+
+def main():
+    args = passed_arguments()
+
+    os.makedirs(args.model_path, exist_ok=True)
+
+    device = torch.cuda.current_device() if torch.cuda.is_available() else 'cpu'
+    tokenizer = AutoTokenizer.from_pretrained("allenai/macaw-large")
+    model = AutoModelForSeq2SeqLM.from_pretrained("allenai/macaw-large")
+    if args.adapter:
+        model.add_adapter("beliefbank", config="pfeiffer")
+        model.set_active_adapters("beliefbank")
+    model = model.to(device)
+    # model = torch.nn.DataParallel(model).to(device)
+
+    train_dataset = QADataset(args.train_path, tokenizer)
+
+    config = {
+        'device': device,
+        'max_epochs': args.max_epochs,
+        'batch_size': args.batch_size,
+        'learning_rate': args.lr,
+        'betas': (0.9, 0.95),
+        'grad_norm_clip': 1.0,
+        'weight_decay': args.weight_decay,  # only applied on matmul weights
+        'l1_reg': args.l1_reg,
+        'freeze_backbone': args.freeze_backbone,
+        # learning rate decay params: linear warmup followed by cosine decay to 10% of original
+        'lr_decay': args.lr_decay,
+        # checkpoint settings
+        'model_path': args.model_path,
+        'num_workers': args.num_workers,  # for DataLoader
+        'adapter': args.adapter,
+        'layer_names': args.layer_names
+    }
+    train(model, train_dataset, config)
+
+if __name__ == "__main__":
+    main()
