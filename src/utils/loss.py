6143a5e06afb26007edcc59a834260f893e54ba9
sconsul <sarthakc@stanford.edu>
2022-03-04 19:12:38 -0800

    tweak in loss calculation, removed comparison of 2nd q with negs

--- src/utils/loss.py
+++ src/utils/loss.py
@@ -6,8 +6,9 @@ import numpy as np
 def binary_sim_loss(batch):
     batch = F.normalize(batch,dim=1) #shape (B,L)
     dot_prods = batch@batch.T #shape (B,B), stores all the dot products of every combination
-    loss = (torch.sum(dot_prods))/2.0
+    sim = torch.sum(dot_prods,dim=0)
+    loss = 0.0
     for i in range(0,len(batch),2):
-        loss -= 2*dot_prods[i][i+1]
+        loss += sim[i] -2*dot_prods[i][i+1]
 
     return loss
