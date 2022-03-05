556acdad4c2f6777ea0d3d0713e8d6ba021d3783
Samar Khanna <samar.khanna@gmail.com>
2022-03-04 19:31:34 -0800

    Similarity loss doc

--- src/utils/loss.py
+++ src/utils/loss.py
@@ -4,11 +4,25 @@ import numpy as np
 
 
 def binary_sim_loss(batch):
-    batch = F.normalize(batch,dim=1) #shape (B,L)
-    dot_prods = batch@batch.T #shape (B,B), stores all the dot products of every combination
-    sim = torch.sum(dot_prods,dim=0)
-    loss = 0.0
-    for i in range(0,len(batch),2):
-        loss += sim[i] -2*dot_prods[i][i+1]
+    """
+    Computes the similarity/difference loss:
+    ∑[i = 0, 2, ..., 2B] (1 - a_i.a_i) + ∑[i=0,1,...]∑[j!=i,j!=i+1] a_i . a_j
+    First sum is -ve cosine similarity of similar questions,
+    second sum is cosine similarity of each question with every other dissimilar question.
+    :param batch: shape (2B, L) of (a_0, a_0', a_1, a_1', ..., a_B, a_B')
+        where consecutive questions are similar
+    :return: Loss value
+    """
+    # Unit norm vectors
+    batch = F.normalize(batch, dim=1)  # shape (B,L)
+
+    # stores all the dot products of every combination
+    dot_prods = batch @ batch.T  # shape (B,B)
+
+    # Sum all similarity, but overcounting 2 + a_i.a_{i+1},
+    # we want 1 - a_i.a_{i+1}, so add -2*a_i.a_{i+1} - 1
+    loss = torch.sum(dot_prods)
+    for i in range(0, len(batch), 2):
+        loss += -2 * dot_prods[i][i + 1] - 1
 
     return loss
