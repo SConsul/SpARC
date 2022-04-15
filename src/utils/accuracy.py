f721eb24d11b5883276706d39ff4ffd442a41ae7
Samar Khanna <samar.khanna@gmail.com>
2022-04-14 22:54:30 -0700

    New version of f1

--- src/utils/accuracy.py
+++ src/utils/accuracy.py
@@ -1,31 +1,36 @@
 import json
 import argparse
 
-def F1_score(results):
-    res = json.load(open(results))
+
+def f1_score(results):
     TP = 0.0
     TN = 0.0
     FP = 0.0
     FN = 0.0
     skip_count = 0
-    for pred in res:
-        if pred['pred'].split()[2][:3]=="Yes" or pred['pred'].split()[2][:3]=="yes":
-            if pred['tgt']=='yes':
-                TP+=1
+    for pred in results:
+        if pred["pred"] == "yes":
+            if pred["answer"] == "yes":
+                TP += 1
             else:
-                FP+=1
-        elif pred['pred'].split()[2][:2]=="No" or pred['pred'].split()[2][:2]=="no":
-            if pred['tgt']=='yes':
-                FN+=1
+                FP += 1
+        elif pred["pred"] == "no":
+            if pred["answer"] == "yes":
+                FN += 1
             else:
-                TN+=1
+                TN += 1
         else:
-            skip_count+=1
-    return TP/(TP+0.5*(FP+FN)), skip_count
+            skip_count += 1
+    return TP / (TP + 0.5 * (FP + FN)), skip_count
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--results_path', default="./beliefbank-data-sep2021/baseline.json")
     args = parser.parse_args()
-    f1, skip_cnt = F1_score(args.results_path)
-    print("F1 Score=",f1,"skipping",skip_cnt)
+
+    with open(args.results_path, 'r') as f:
+        results = json.load(f)
+
+    f1, skip_cnt = f1_score(results)
+    print("F1 Score=", f1, "skipping", skip_cnt)
