dbecaa3da57cb3aab05a6f924afcb050e891231c
Samar Khanna <samar.khanna@gmail.com>
2022-04-14 21:40:51 -0700

    refactored to use data row for storing preds

--- src/inference.py
+++ src/inference.py
@@ -3,9 +3,10 @@ import argparse
 from tqdm import tqdm
 import torch
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-from torch.utils.data import Dataset
 from torch.utils.data.dataloader import DataLoader
+
 from utils.dataset import QADataset
+from utils.preprocess_utils import DataRow
 
 
 def batch(iterable, n=1):
@@ -14,18 +15,49 @@ def batch(iterable, n=1):
         yield iterable[ndx:min(ndx + n, l)]
 
 
-def infer():
+def infer(model, test_dataset, qa, batch_size, device):
+
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, drop_last=False)
+    pbar = tqdm(zip(test_dataloader, batch(qa, batch_size)), total=len(test_dataloader))
+
+    output_preds = []
+    for inputs, data_text in pbar:
+        q_ids, attn, _, _ = inputs
+        q_ids = q_ids.to(device)
+        attn = attn.to(device)
+
+        output_sequences = model.generate(
+            input_ids=q_ids.squeeze(1),  # (b, 1, L) -> (b, L)
+            attention_mask=attn.squeeze(1),  # (b, 1, L) -> (b, L)
+            do_sample=False,  # disable sampling to test if batching affects output
+        )
+
+        preds = tokenizer.batch_decode(output_sequences, skip_special_tokens=True)
+
+        # q_s = tokenizer.batch_decode(q, skip_special_tokens=True)
+        # y[y==-100] = tokenizer.pad_token_id
+        # a_s = tokenizer.batch_decode(y, skip_special_tokens=True)
+        for pred, data_row in zip(preds, data_text):
+            output_preds.append(
+                DataRow(question=data_row['question'], answer=data_row['answer'],
+                        source=data_row['source'], target=data_row['target'],
+                        gold=False, pred=pred)
+            )
+
+    return output_preds
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--in_path', default="./beliefbank-data-sep2021/qa.json")
+    parser.add_argument('--in_path', default="./beliefbank-data-sep2021/qa_val.json")
     parser.add_argument('--out_path', default="./beliefbank-data-sep2021/baseline.json")
     parser.add_argument('--model_path', default=None, type=str)
-    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--batch_size', type=int, default=512)
     parser.add_argument('--adapter', action='store_true', default=False)
     args = parser.parse_args()
 
     device = torch.cuda.current_device() if torch.cuda.is_available() else 'cpu'
 
-    output_preds = []
     tokenizer = AutoTokenizer.from_pretrained("allenai/macaw-large")
     model = AutoModelForSeq2SeqLM.from_pretrained("allenai/macaw-large")
     if args.adapter:
@@ -40,34 +72,9 @@ def infer():
         qa = json.load(f)
 
     test_dataset = QADataset(args.in_path, tokenizer)
-    test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
-    pbar = tqdm(zip(test_dataloader, batch(qa, args.batch_size)), total=len(test_dataloader))
 
-    output_preds = []
-    for dl, dc in pbar:
-        q_ids, attn, _, _ = dl
-        q_ids = q_ids.to(device)
-        attn = attn.to(device)
+    output_preds = infer(model, test_dataset, qa, args.batch_size, device)
 
-        output_sequences = model.generate(
-            input_ids=q_ids.squeeze(1),  # (b, 1, L) -> (b, L)
-            attention_mask=attn.squeeze(1),  # (b, 1, L) -> (b, L)
-            do_sample=False,  # disable sampling to test if batching affects output
-        )
-
-        preds = tokenizer.batch_decode(output_sequences, skip_special_tokens=True)
-        q_s = [d['question'] for d in dc]
-        a_s = [d['answer'] for d in dc]
-        # q_s = tokenizer.batch_decode(q, skip_special_tokens=True)
-        # y[y==-100] = tokenizer.pad_token_id
-        # a_s = tokenizer.batch_decode(y, skip_special_tokens=True)        
-        data = [{'q': q, 'pred': pred, 'tgt': a} for (q, pred, a) in zip(q_s, preds, a_s)]
-        output_preds = output_preds + data
-
+    output_preds = [dr._asdict() for dr in output_preds]
     with open(args.out_path, 'w') as outfile:
-        json.dump(output_preds, outfile, indent=4)
-    outfile.close()
-
-
-if __name__ == "__main__":
-    infer()
+        json.dump(output_preds, outfile, indent=1)
